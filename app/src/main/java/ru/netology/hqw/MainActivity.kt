75ccd02ff67a37fa4d24a006d724eaae2e0d2a2d
NikitaBarkanov <108590321+NikitaBarkanov@users.noreply.github.com>
2022-09-22 00:38:33 +0300

    Q

--- app/src/main/java/ru/netology/hqw/MainActivity.kt
+++ app/src/main/java/ru/netology/hqw/MainActivity.kt
@@ -2,10 +2,11 @@ package ru.netology.hqw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.netology.hqw.R.layout.activity_main
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activity_main)
     }
 }
