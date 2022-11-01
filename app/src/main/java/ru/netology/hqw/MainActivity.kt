af1b65e422ab1244ebd11cb21a3475f55bf68829
NikitaBarkanov <108590321+NikitaBarkanov@users.noreply.github.com>
2022-11-01 11:07:27 +0300

    2.4

--- app/src/main/java/ru/netology/hqw/MainActivity.kt
+++ app/src/main/java/ru/netology/hqw/MainActivity.kt
@@ -4,15 +4,9 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import ru.netology.hqw.R.layout.activity_main
 import ru.netology.hqw.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var textView: TextView
-
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
