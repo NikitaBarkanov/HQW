504ba4006fcb04292a6949ed60b42bebd30c0ee9
NikitaBarkanov <108590321+NikitaBarkanov@users.noreply.github.com>
2023-01-27 10:35:18 +0300

    Notifications

---
+++ app/src/main/java/ru/netology/hqw/service/FCMService.kt
@@ -0,0 +1,151 @@
+package ru.netology.hqw.service
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.google.gson.Gson
+import ru.netology.hqw.R
+import ru.netology.hqw.dto.Post
+import java.lang.IllegalArgumentException
+import kotlin.random.Random
+
+class FCMService : FirebaseMessagingService() {
+    private val action = "action"
+    private val content = "content"
+    private val channelId = "remote"
+    private val gson = Gson()
+
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_remote_name)
+            val descriptionText = getString(R.string.channel_remote_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+
+        message.data[action]?.let {
+            try {
+                when (Action.valueOf(it)) {
+                    Action.LIKE -> handleLike(
+                        gson.fromJson(
+                            message.data[content],
+                            Like::class.java
+                        )
+                    )
+                    Action.POST -> handlePost(
+                        gson.fromJson(message.data[content],
+                            Post::class.java
+                        )
+                    )
+                    Action.EMPTY ->  Log.e(
+                        "push sender",
+                        "wrong message -> please send this log to server tech support"
+                    )
+                }
+            } catch (error: IllegalArgumentException){
+                Log.e(
+                    "push sender",
+                    "wrong enum -> please send this log to server tech support"
+                )
+            }
+        }
+    }
+
+    override fun onNewToken(token: String) {
+        println(token)
+    }
+
+    @SuppressLint("StringFormatInvalid")
+    private fun handleLike(content: Like) {
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notifications)
+            .setContentTitle(
+                getString(
+                    R.string.notification_user_liked,
+                    content.userName,
+                    content.postAuthor,
+                )
+            )
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        NotificationManagerCompat.from(this)
+            .notify(Random.nextInt(100_000), notification)
+    }
+
+    @SuppressLint("StringFormatInvalid")
+    private fun handlePost (content: Post){
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notifications)
+            .setContentTitle(
+                getString(
+                    R.string.notification_user_published,
+                    content.author
+                )
+            )
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setStyle(NotificationCompat.BigTextStyle()
+                .bigText(content.content))
+            .build()
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        NotificationManagerCompat.from(this)
+            .notify((Random.nextInt(100_000)+31), notification)
+    }
+}
+
+enum class Action {
+    LIKE, POST, EMPTY
+}
+
+data class Like(
+    val userId: Long,
+    val userName: String,
+    val postId: Long,
+    val postAuthor: String,
+)
